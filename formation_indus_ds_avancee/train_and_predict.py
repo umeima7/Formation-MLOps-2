ab48481fa6bd9207051b69187f85aa1eda5447e8
umeima7 <doulazmi.oumaima@gmail.com>
2026-01-29 15:21:29 +0000

    model registry mlflow

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -5,6 +5,7 @@ import joblib
 import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
@@ -18,9 +19,15 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     X = features.drop(columns=[target])
     y = features[target]
     with mlflow.start_run():
-        # insert autolog here ...
+        mlflow.set_tag("mlflow.runName", "random_forest_train_v1")
+        mlflow.sklearn.autolog()
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
+        mlflow.sklearn.log_model(sk_model=model,
+            artifact_path="model",
+            registered_model_name="RandomForest_Ba_avg")
+        
+
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
 
